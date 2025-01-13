1fa217791c094d7e14bd2be2888c220f30afdb3a
jtappi <bhattjk@gmail.com>
2025-01-13 01:32:16 -0500

    moving scripts around and getting rid of errors

---
+++ public/navigation.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const backToLoginBtn = document.getElementById('backToLoginBtn');
+    if (backToLoginBtn) {
+        backToLoginBtn.addEventListener('click', () => {
+            window.location.href = '/login.html';
+        });
+    }
+
+    const logoutBtn = document.getElementById('logoutBtn');
+    if (logoutBtn) {
+        logoutBtn.addEventListener('click', () => {
+            // Clear authentication tokens or session data
+            localStorage.removeItem('authToken');
+            sessionStorage.removeItem('authToken');
+            // Delete the connect.sid cookie for trackmyweek.com
+            document.cookie = "connect.sid=;expires=" + new Date(0).toUTCString() + ";path=/;domain=trackmyweek.com;secure";
+            console.log('Attempted to delete connect.sid cookie for trackmyweek.com');
+            // Redirect to login page
+            window.location.href = '/login.html';
+        });
+    }
+
+    const analyzeDataBtn = document.getElementById('analyzeDataBtn');
+    if (analyzeDataBtn) {
+        analyzeDataBtn.addEventListener('click', () => {
+            window.location.href = '/analyze-data.html'; // Adjust the URL as needed
+        });
+    }
+
+    const viewAllDataBtn = document.getElementById('viewAllDataBtn');
+    if (viewAllDataBtn) {
+        viewAllDataBtn.addEventListener('click', () => {
+            window.location.href = 'view-data.html';
+        });
+    }
+});
--- public/script.js
+++ public/script.js
@@ -1,41 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const backToLoginBtn = document.getElementById('backToLoginBtn');
-    if (backToLoginBtn) {
-        backToLoginBtn.addEventListener('click', () => {
-            window.location.href = '/login.html';
-        });
-    }
-
-    const logoutBtn = document.getElementById('logoutBtn');
-    if (logoutBtn) {
-        logoutBtn.addEventListener('click', () => {
-            // Clear authentication tokens or session data and cookies
-            localStorage.removeItem('authToken');
-            sessionStorage.removeItem('authToken');
-            
-            // Delete the connect.sid cookie for trackmyweek.com
-            document.cookie = "connect.sid=;expires=" + new Date(0).toUTCString() + ";path=/;domain=trackmyweek.com;secure";
-            console.log('Attempted to delete connect.sid cookie for trackmyweek.com');
-
-            // Redirect to login page
-            window.location.href = '/login.html';
-    });
-    }
-
-    const analyzeDataBtn = document.getElementById('analyzeDataBtn');
-    if (analyzeDataBtn) {
-        analyzeDataBtn.addEventListener('click', () => {
-            window.location.href = '/analyze-data.html'; // Adjust the URL as needed
-        });
-    }
-
-    const viewAllDataBtn = document.getElementById('viewAllDataBtn');
-    if (viewAllDataBtn) {
-        viewAllDataBtn.addEventListener('click', () => {
-            window.location.href = '/view-data.html'; // Adjust the URL as needed
-        });
-    }
-
     const submitBtn = document.getElementById('submitBtn');
     if (submitBtn) {
         submitBtn.addEventListener('click', () => {
@@ -160,7 +123,7 @@ document.addEventListener('DOMContentLoaded', () => {
             alert.classList.remove('show');
             alert.classList.add('hide');
             setTimeout(() => alert.remove(), 500);
-        }, 5000);
+        }, 1500);
     }
 
     function submitItem(itemName, selectedCategory, cost, notes, calories) {
